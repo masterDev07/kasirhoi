d3ad6119f5a5fd22e84bf4c4ec91cc09d7d7513b
Endro Mei Asmoro <endropbg@yahoo.com>
2026-06-23 17:05:53 +0700

    Fix remaining two so file

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# Menjalankan aplikasi
-# if __name__ == "__main__":
-    # import kasir_gudang
-    # # Ini hanya simulasi jika dijalankan tanpa login window
-    # app = kasir_gudang.KasirGudangApp("kasir1","kasir", "Slamet", None)
-    # app.mainloop()
-
 if __name__ == "__main__":
     from views.login_window import LoginWindow
     from models.sinkronisasi_model import background_sync_task,luncurkan_aplikasi_kasir
